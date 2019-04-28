8463264fca6d58da12752b99ee30f8c34a76ac27
ashea2120 <ashea2120@yahoo.com>
2019-04-28 10:36:15 -0400

    backend code completely functional

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int numOfCoffees = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        //display(numOfCoffees);
+        displayPrice(numOfCoffees*5);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void addToOrder(View view) {
+        numOfCoffees++;
+        display(numOfCoffees);
+    }
+
+    public void subtractFromOrder(View view) {
+        numOfCoffees--;
+        display(numOfCoffees);
+    }
+}
